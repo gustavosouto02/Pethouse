c726608ed486ee10c88cf7aa83e0c2a710e3e6a8
anaclaracald <ana05clara24@gmail.com>
2025-07-02 17:10:14 -0300

    Card home view finalizado

--- Pethouse/Components/CardHomeView.swift
+++ Pethouse/Components/CardHomeView.swift
@@ -8,18 +8,74 @@
 import SwiftUI
 
 struct CardHomeView: View {
-	let schedule: Schedule
+//	let schedule: Schedule
 	
     var body: some View {
 		VStack(alignment: .leading, spacing: 16){
-			HStack(alignment: .top) {
-				// Foto
-				//
+			HStack(alignment: .center) {
+				
+				// TODO: Foto
+				Circle()
+					.frame(width: 40, height: 40)
+				
+				
+				// TODO: Nome do pet
+				Text("Nome do pet")
+					.font(.subheadline)
+				
+				// TODO: Icone de status do pagamento
+				Button{
+					// TODO: SWITCH CASE PARA ALTERAR STATUS DO PAGAMENTO NESSE BUTTON
+				} label: {
+					Circle()
+						.frame(width: 25, height: 25)
+						.padding(.leading, 8)
+						.padding(.bottom)
+						.foregroundStyle(Color.greenStatus)
+				}
 			}
+			
+			HStack{
+				// TODO: Nome do tutor
+				Text("Tutor: tutor name")
+					.font(.footnote)
+					.foregroundStyle(Color.gray)
+			}
+			
+			HStack {
+				// TODO: data de entrada e saida da estadia
+				Text("Date - date")
+					.frame(width: 110, height: 18)
+					.font(.caption)
+					.padding(.horizontal, 12)
+					.padding(.vertical, 6)
+					.background(Color.accentColor)
+					.foregroundColor(Color.white)
+					.clipShape(Capsule())
+				
+				Image(systemName: "arrowtriangle.right.fill")
+					.frame(width: 12, height: 12)
+					.foregroundStyle(Color.accentColor)
+					.padding(.leading, 15)
+			}
+			
+					
+			
 		}
+		.padding()
+		.frame(width: 200, height: 150)
+		.background(
+			RoundedRectangle(cornerRadius: 20)
+				.fill(Color.white)
+		)
+		.overlay(
+			RoundedRectangle(cornerRadius: 20)
+				.stroke(Color.accentColor, lineWidth: 1)
+		)
+
     }
 }
-//
-//#Preview {
-//    CardHomeView()
-//}
+
+#Preview {
+	CardHomeView()
+}
