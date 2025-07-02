7ef7466610cc1d8d9a5fa272260c7aed192f71f5
Ana Luísa Coleone <isacoleone27@gmail.com>
2025-07-02 17:00:57 -0300

    Iniciando ViewModel

---
+++ Pethouse/View/PetTutorView.swift
@@ -0,0 +1,18 @@
+//
+//  PetTutorView.swift
+//  Pethouse
+//
+//  Created by Ana Luisa Teixeira Coleone Reis on 02/07/25.
+//
+
+import SwiftUI
+
+struct PetTutorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PetTutorView()
+}
---
+++ Pethouse/ViewModel/PetTutorViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PetTutorViewModel.swift
+//  Pethouse
+//
+//  Created by Ana Luisa Teixeira Coleone Reis on 02/07/25.
+//
+
+import Foundation
