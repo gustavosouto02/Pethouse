8953025a677bcc0ab6d3802b690d49ff1366e104
Ana Luísa Coleone <isacoleone27@gmail.com>
2025-07-02 17:02:20 -0300

    Iniciando View

--- Pethouse/HomeView.swift
+++ Pethouse/HomeView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
     }
 }
 
--- Pethouse/ViewModel/PetTutorViewModel.swift
+++ Pethouse/ViewModel/PetTutorViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import SwiftData
+
+extension PetTutorView {
+    
+    @Observable
+    class PetTutorViewModel {
+        var context: ModelContext
+        var pets: [Pet] = []
+        var tutors: [Tutor] = []
+        
+        init(context: ModelContext) {
+            self.context = context
+            fetchAllPetAndTutors()
+        }
+        
+        func fetchAllPetAndTutors() {
+            do {
+                let petDescriptor = FetchDescriptor<Pet>(sortBy: [SortDescriptor(\.name)])
+                let tutorDescriptor = FetchDescriptor<Tutor>(sortBy: [SortDescriptor(\.name)])
+                
+                pets = try context.fetch(petDescriptor)
+                tutors = try context.fetch(tutorDescriptor)
+                
+            } catch {
+                print("Fetch pet and tutors failed. \(error)")
+            }
+        }
+        
+        func addPet(name: String, age: Int, breed: String, specie: String, gender: String) {
+            let newPet = Pet(name: name, age: age, breed: breed, specie: specie, gender: gender)
+            
+            context.insert(newPet)
+            fetchAllPetAndTutors()
+        }
+        
+        func addTutor(name: String, cpf: String, phone: String) {
+            let newTutor = Tutor(name: name, cpf: cpf, phone: phone)
+            
+            context.insert(newTutor)
+            fetchAllPetAndTutors()
+        }
+    }
+}
