95c66a4ba88d9a0c414ab218e4cc51d4f22b347a
anaclaracald <ana05clara24@gmail.com>
2025-07-02 15:40:40 -0300

    Home card

---
+++ Pethouse/Components/CardHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  CardHomeView.swift
+//  Pethouse
+//
+//  Created by Ana Clara Ferreira Caldeira on 02/07/25.
+//
+
+import SwiftUI
+
+struct CardHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardHomeView()
+}
