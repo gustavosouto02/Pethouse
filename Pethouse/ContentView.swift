19617b9b4cd4d3ae64e28198603e2cdaf23388c4
gustavosouto02 <gugasouto2004@gmail.com>
2025-07-02 14:09:37 -0300

    Initial Commit

---
+++ Pethouse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pethouse
+//
+//  Created by Gustavo Souto Pereira on 02/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pethouse/PethouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PethouseApp.swift
+//  Pethouse
+//
+//  Created by Gustavo Souto Pereira on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PethouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
