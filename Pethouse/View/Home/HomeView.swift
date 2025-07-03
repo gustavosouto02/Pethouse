f3c75d860c00ec79970c111b0727e214e4eaea4a
thiagocomth <thiago8jesusaraujo@gmail.com>
2025-07-03 17:21:22 -0300

    rebase

--- Pethouse/View/Home/HomeView.swift
+++ Pethouse/View/Home/HomeView.swift
@@ -13,6 +13,7 @@ struct HomeView: View {
     @State var viewModel = HomeViewModel()
     @AppStorage("didInsertDefaultReflections") var didInsertDefaults = false
     @Environment(\.modelContext) var context
+    @State var showCardView: Bool = false
     
     @Query var schedules: [Schedule]
     
@@ -23,76 +24,74 @@ struct HomeView: View {
         return schedules.filter { $0.entryDate <= Date() }
     }
 
-	@State var showCardView = false
     var body: some View {
         NavigationStack {
-			VStack(alignment: .leading) {
-					Text("Estadias atuais")
-						.font(.headline)
-					ScrollView (.horizontal, showsIndicators: false){
-				Text("Estadias atuais")
-					.font(.headline)
-					.padding(.leading)
-					.padding(.top, 20)
-				ScrollView (.horizontal, showsIndicators: false){
-					HStack {
-						ForEach(currentSchedules) { schedule in
-							Button {
-								showCardView = true
-							} label: {
-								CardHomeView(schedule: schedule)
-									.shadow(color: Color.gray.opacity(0.3) ,radius: 5)
-									.padding(10)
-							}
-						}
-					}
-				}
-				.scrollTargetLayout()
-				.padding(.bottom, 20)
+            VStack(alignment: .leading) {
+                Text("Estadias atuais")
+                    .font(.headline)
+                    .padding(.leading)
+                    .padding(.top, 20)
 
-				Text("Estadias futuras")
-					.font(.headline)
-					.padding(.leading)
-				ScrollView (.horizontal, showsIndicators: false) {
-					HStack {
-						ForEach(futureSchedules) { schedule in
-							Button {
-								showCardView = true
-							} label: {
-								CardHomeView(schedule: schedule)
-									.shadow(color: Color.gray.opacity(0.3) ,radius: 5)
-									.padding(10)
-							}
-						}
-					}
-				}
-				.scrollTargetLayout()
-			
-			Spacer()
-			
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(currentSchedules) { schedule in
+                            Button {
+                                showCardView = true
+                            } label: {
+                                CardHomeView(schedule: schedule)
+                                    .shadow(color: Color.gray.opacity(0.3), radius: 5)
+                                    .padding(10)
+                            }
+                        }
+                    }
+                    .scrollTargetLayout()
+                    .padding(.bottom, 20)
+                }
+
+                Text("Estadias futuras")
+                    .font(.headline)
+                    .padding(.leading)
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(futureSchedules) { schedule in
+                            CardHomeView(schedule: schedule)
+                                .shadow(color: Color.gray.opacity(0.3), radius: 5)
+                                .padding(10)
+                        }
+                    }
+                    .scrollTargetLayout()
+                }
+
+                Spacer()
             }
             .navigationTitle("PetHouse")
-            .onAppear(){
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        viewModel.showAddSchedule = true
+                    } label: {
+                        AddScheduleButton()
+                    }
+                }
+            }
+            .navigationDestination(isPresented: $viewModel.showAddSchedule) {
+                AddScheduleView()
+            }
+            .navigationDestination(isPresented: $showCardView) {
+                // TODO: colocar os atributos para quando abrir pelo showCardView ele mostrar os dados do card ja cadastrado
+                AddScheduleView()
+            }
+            .onAppear {
                 if !didInsertDefaults {
                     viewModel.mock.insertMockDataIfNeeded(context: context)
                     didInsertDefaults = true
                 }
             }
-                    }
-                    
-                }
-            }
-            .navigationDestination(isPresented: $viewModel.showAddSchedule, destination: {
-                AddScheduleView()
-            })
-	
-			.navigationDestination(isPresented: $showCardView, destination: {
-				//TODO: colocar os atributos para quando abrir pelo showCardView ele mostrar os dados do card ja cadastrado
-				AddScheduleView()
-			})
         }
-	}
-
-	
-        //.padding()
     }
+}
+
+#Preview {
+    HomeView()
+}
